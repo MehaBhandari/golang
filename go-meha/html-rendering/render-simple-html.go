f05f6e80c5bd75aacab41bcd27007adac587ee72
Meha <ahem4progs@gmail.com>
2022-06-09 16:58:03 +0530

    HTML rendering & anchor tag redirection to other page

--- go-meha/html-rendering/render-simple-html.go
+++ go-meha/html-rendering/render-simple-html.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", ShowHtmlTemplate)
+	http.HandleFunc("/about-Techno-Funnel", AboutTechnofunnel)
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("appScripts"))))
 	http.ListenAndServe(":8000", nil)
@@ -17,9 +18,20 @@ func main() {
 // localhost:8000/css/index.css
 
 func ShowHtmlTemplate(response http.ResponseWriter, request *http.Request) {
+	filePath := path.Join("html-pages","index.html")
+	/* 
+	In case of same folder
 	filePath := path.Join("index.html")
+	*/
 	fmt.Println(filePath)
 	applicationTemplate, _ := template.ParseFiles(filePath)
 	applicationTemplate.Execute(response, nil)
 }
 
+
+func AboutTechnofunnel(response http.ResponseWriter, request *http.Request) {
+	filepath := path.Join("html-pages", "about.html")
+	fmt.Println(filepath)
+	applicationTemplate, _ :=template.ParseFiles(filepath)
+	applicationTemplate.Execute(response, nil)
+}
