c260661a448656801867b526880414093fabfdd9
Meha <ahem4progs@gmail.com>
2022-06-10 09:37:53 +0530

    created server file

--- go-meha/http-server/create-server.go
+++ go-meha/http-server/create-server.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/simplePrint", PrintSimpleString)
+type Employee struct {
+	Name string
+	Age byte
 }
 
-func PrintSimpleString(w http.Response,r *http.Request) {
-	
+func main() {
+	http.HandleFunc("/", PrintSimpleString)
+	http.HandleFunc("/showJsonData", PrintSimpleJson)
+	http.ListenAndServe(":4000", nil)
 }
+
+func PrintSimpleString(w http.ResponseWriter, r *http.Request) {
+	var userName = "Meha Bhandari"
+	fmt.Fprintf(w , "Hello " +userName)
+}
+
+func PrintSimpleJson(w http.ResponseWriter, r *http.Request) {
+	emp := Employee{"Gauri", 5}
+	dataOutput, _ := json.Marshal(emp)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(dataOutput)
+}
+
+// func ServerDataFiles(w http.ResponseWriter, r *http.Request) {
+// 	http.ListenAndServe(":3000", http.FileServer(http.Dir("")))
+// }
